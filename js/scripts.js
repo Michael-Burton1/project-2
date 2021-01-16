00206113901fea157cab541e43a7ee1a0fca4cdb
Michael Burton <micbur1@gmail.com>
2021-01-15 16:12:34 -0800

    add const values to questions.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  const quest1Ans=$("#quest1").val();
+  const quest2Ans=$("#quest2").val();
+  const quest3Ans=$("#quest3").val();
+  const quest4Ans=$("#quest4").val();
+  const quest5Ans=$("#quest5").val();
+
+}
