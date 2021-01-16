d0e44639e4e1855e50f9ceb7899bd848a64ad61a
Michael Burton <micbur1@gmail.com>
2021-01-15 19:15:08 -0800

    add prevent default function in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,24 @@
 $(document).ready(function(){
-
-  const quest1Ans=$("#quest1").val();
-  const quest2Ans=$("#quest2").val();
-  const quest3Ans=$("#quest3").val();
-  const quest4Ans=$("#quest4").val();
-  const quest5Ans=parseInt($("#quest5").val());
-  // alert(quest5Ans) 
-
-  if(quest5==="1"){
+  $("#form1").submit(function(e){
+    e.preventDefault();
+    const quest5Ans=parseInt($("#quest5").val());
+  });
+   alert(quest5Ans) 
+  // let quest5=5
+  if(quest5Ans==="1"){
     $('.javaScriptResults').show();
-  }else if (quest5===2){
+  }else if (quest5Ans===2){
       $('.rubyResult').show();
-  }else if (quest5===3){
+  }else if (quest5Ans===3){
     $('cResult').show();
-  }else if (quest5===4){
+  }else if (quest5Ans===4){
     $('.confussedResult').show();
-  }else if (quest5===5){
+  }else if (quest5Ans===5){
     $('.danceResult').show();
   }
+  // else{
+    // (".results").hide();
+  // }
     
   
 
