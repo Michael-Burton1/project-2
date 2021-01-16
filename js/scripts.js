3054879a5d631e7168eb9818989113c23cd6def0
Michael Burton <micbur1@gmail.com>
2021-01-15 20:14:47 -0800

    move if else statements inside submit function and correct class names in function

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,24 @@ $(document).ready(function(){
   $("#form1").submit(function(e){
     e.preventDefault();
     const quest5Ans=parseInt($("#quest5").val());
-  });
-   alert(quest5Ans) 
-  // let quest5=5
-  if(quest5Ans==="1"){
-    $('.javaScriptResults').show();
-  }else if (quest5Ans===2){
+    console.log($("#quest5").val())
+    // alert(quest5Ans) 
+    // let quest5=5
+    if(quest5Ans===1){
+      $('.javaScriptResult').show();
+    }else if (quest5Ans===2){
       $('.rubyResult').show();
-  }else if (quest5Ans===3){
-    $('cResult').show();
-  }else if (quest5Ans===4){
-    $('.confussedResult').show();
-  }else if (quest5Ans===5){
-    $('.danceResult').show();
-  }
-  // else{
-    // (".results").hide();
-  // }
+    }else if (quest5Ans===3){
+      $('.cResult').show();
+    }else if (quest5Ans===4){
+      $('.confussedResult').show();
+    }else if (quest5Ans===5){
+      $('.danceResult').show();
+    }
+    // else{
+      // (".results").hide();
+      // }
+    });
     
   
 
