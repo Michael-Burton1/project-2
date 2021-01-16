86ebecdafb29aa03beb8c906452defaabcc004c8
Michael Burton <micbur1@gmail.com>
2021-01-15 17:00:33 -0800

    add if else statements to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,21 @@ $(document).ready(function(){
   const quest2Ans=$("#quest2").val();
   const quest3Ans=$("#quest3").val();
   const quest4Ans=$("#quest4").val();
-  const quest5Ans=$("#quest5").val();
-  alert(quest5Ans) 
+  const quest5Ans=parseInt($("#quest5").val());
+  // alert(quest5Ans) 
+
+  if(quest5===1){
+    $('.JavaScriptResult').show();
+  }else if (quest5===2){
+      $('.rubyResult').show();
+  }else if (quest5===3){
+    $('c#Result').show();
+  }else if (quest5===4){
+    $('.confussedResult').show();
+  }else if (quest5===5){
+    $('.danceResult').show();
+  }
+    
+  
+
 });
