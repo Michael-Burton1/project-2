dc1232041acc5689d9db3bd0b3c76ea1872c4b6d
Michael Burton <micbur1@gmail.com>
2021-01-15 18:08:05 -0800

    debugged img and worked on functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   const quest1Ans=$("#quest1").val();
   const quest2Ans=$("#quest2").val();
   const quest3Ans=$("#quest3").val();
@@ -6,12 +7,12 @@ $(document).ready(function(){
   const quest5Ans=parseInt($("#quest5").val());
   // alert(quest5Ans) 
 
-  if(quest5===1){
-    $('.JavaScriptResult').show();
+  if(quest5==="1"){
+    $('.javaScriptResults').show();
   }else if (quest5===2){
       $('.rubyResult').show();
   }else if (quest5===3){
-    $('c#Result').show();
+    $('cResult').show();
   }else if (quest5===4){
     $('.confussedResult').show();
   }else if (quest5===5){
